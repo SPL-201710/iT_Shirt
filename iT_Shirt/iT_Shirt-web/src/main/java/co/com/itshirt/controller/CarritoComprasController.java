23107f99ab1b40d5da5a00af48660e8b8c355aae
GERMAN <gchicaduque@gmail.com>
2017-05-09 01:22:23 -0500

    Eliminación de ordenes de compra del carrito

--- iT_Shirt/iT_Shirt-web/src/main/java/co/com/itshirt/controller/CarritoComprasController.java
+++ iT_Shirt/iT_Shirt-web/src/main/java/co/com/itshirt/controller/CarritoComprasController.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import co.com.itshirt.domain.DetalleOrden;
+import co.com.itshirt.domain.Estampa;
 import co.com.itshirt.domain.OrdenCompra;
 import co.com.itshirt.enums.EnumEstadoCompra;
 import co.com.itshirt.repository.DetalleOrdenRepository;
@@ -46,6 +50,15 @@ public class CarritoComprasController {
 		session.setAttribute("envioEstimado", envioEstimado);
 		session.setAttribute("total", total);
 		return "compra/carrito";
+	}
+	
+	@RequestMapping(value= "eliminarOrden", method = RequestMethod.POST)
+	public String eliminarOrden(@RequestParam(value="ordDet", required=true) int ordDet, Model model, HttpSession session, final RedirectAttributes redirectAttributes) {
+		if (session.getAttribute("elementosCarrito")!=null) {
+			ArrayList<DetalleOrden> elementosCarrito = (ArrayList<DetalleOrden>) session.getAttribute("elementosCarrito");
+			elementosCarrito.remove(ordDet-1);
+		}
+		return "redirect:/carrito";
 	}
 	
 	@RequestMapping(value= "metodoPago", method = RequestMethod.GET)
